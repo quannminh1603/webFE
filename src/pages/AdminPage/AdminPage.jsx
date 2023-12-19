5c3dcfdab02774123c10857c12206ce16fa60667
kieu.hoa <kieuhoa1105vn@gmail.com>
2023-12-19 23:26:48 +0700

    add tab

--- src/pages/AdminPage/AdminPage.jsx
+++ src/pages/AdminPage/AdminPage.jsx
@@ -124,7 +124,7 @@
 
 
 // test
-import { Menu } from 'antd'
+import {Menu, Tabs} from 'antd'
 import React, { useEffect, useState } from 'react'
 import { getItem } from '../../utils';
 import { UserOutlined, AppstoreOutlined, ShoppingCartOutlined } from '@ant-design/icons'
@@ -135,113 +135,138 @@ import OrderAdmin from '../../components/OrderAdmin/OrderAmin';
 import * as OrderService from '../../services/OrderService'
 import * as ProductService from '../../services/ProductService'
 import * as UserService from '../../services/UserService'
+import * as CategoryService from '../../services/CategoryService'; // Thêm service danh mục
 
 import CustomizedContent from './components/CustomizedContent';
 import { useSelector } from 'react-redux';
 import { useQueries } from '@tanstack/react-query';
 import { useMemo } from 'react';
 import Loading from '../../components/LoadingComponent/Loading';
+import CategoryList from "./Category/CategoryList";
 
 const AdminPage = () => {
-  const user = useSelector((state) => state?.user)
+    const user = useSelector((state) => state?.user);
 
-  const items = [
-    getItem('Người dùng', 'users', <UserOutlined />),
-    getItem('Sản phẩm', 'products', <AppstoreOutlined />),
-    getItem('Đơn hàng', 'orders', <ShoppingCartOutlined />),
-    
-  ];
+    const items = [
+        getItem('Người dùng', 'users', <UserOutlined />),
+        getItem('Sản phẩm', 'products', <AppstoreOutlined />),
+        getItem('Đơn hàng', 'orders', <ShoppingCartOutlined />),
+        getItem('Danh mục', 'categories', <CategoryList />), // Thêm tab danh mục
+    ];
 
-  const [keySelected, setKeySelected] = useState('');
-  const getAllOrder = async () => {
-    const res = await OrderService.getAllOrder(user?.access_token)
-    return {data: res?.data, key: 'orders'}
-  }
+    const [keySelected, setKeySelected] = useState('');
+    const getAllOrder = async () => {
+        const res = await OrderService.getAllOrder(user?.access_token);
+        return { data: res?.data, key: 'orders' };
+    };
 
-  const getAllProducts = async () => {
-    const res = await ProductService.getAllProduct()
-    return {data: res?.data, key: 'products'}
-  }
+    const getAllProducts = async () => {
+        const res = await ProductService.getAllProduct();
+        return { data: res?.data, key: 'products' };
+    };
 
-  const getAllUsers = async () => {
-    const res = await UserService.getAllUser(user?.access_token)
-    return {data: res?.data, key: 'users'}
-  }
+    const getAllUsers = async () => {
+        const res = await UserService.getAllUser(user?.access_token);
+        return { data: res?.data, key: 'users' };
+    };
 
-  const queries = useQueries({
-    queries: [
-      {queryKey: ['products'], queryFn: getAllProducts, staleTime: 1000 * 60},
-      {queryKey: ['users'], queryFn: getAllUsers, staleTime: 1000 * 60},
-      {queryKey: ['orders'], queryFn: getAllOrder, staleTime: 1000 * 60},
-    ]
-  })
-  const memoCount = useMemo(() => {
-    const result = {}
-    try {
-      if(queries) {
-        queries.forEach((query) => {
-          result[query?.data?.key] = query?.data?.data?.length
-        })
-      }
-    return result
-    } catch (error) {
-      return result
-    }
-  },[queries])
-  const COLORS = {
-   users: ['#e66465', '#9198e5'],
-   products: ['#a8c0ff', '#3f2b96'],
-   orders: ['#11998e', '#38ef7d'],
-  };
+    const getCategoryList = async () => {
+        try {
+            const res = await CategoryService.getAllCategory();
+            return { data: res?.data, key: 'categories' };
+        } catch (error) {
+            console.error('Error fetching category list:', error);
+            return { data: [], key: 'categories' };
+        }
+    };
 
-  const renderPage = (key) => {
-    switch (key) {
-      case 'users':
-        return (
-          <AdminUser />
-        )
-      case 'products':
-        return (
-          <AdminProduct />
-        )
-      case 'orders':
-        return (
-          <OrderAdmin />
-        )
-      default:
-        return <></>
-    }
-  }
+    const queries = useQueries({
+        queries: [
+            { queryKey: ['products'], queryFn: getAllProducts, staleTime: 1000 * 60 },
+            { queryKey: ['users'], queryFn: getAllUsers, staleTime: 1000 * 60 },
+            { queryKey: ['orders'], queryFn: getAllOrder, staleTime: 1000 * 60 },
+            { queryKey: ['categories'], queryFn: getCategoryList, staleTime: 1000 * 60 }, // Thêm query cho danh mục
+        ],
+    });
 
-  const handleOnCLick = ({ key }) => {
-    setKeySelected(key)
-  }
-  return (
-    <>
-      <HeaderComponent isHiddenSearch isHiddenCart />
-      <div style={{ display: 'flex',overflowX: 'hidden', marginTop: '92px' }}>
-        <Menu
-          mode="inline"
-          style={{
-            width: 256,
-            boxShadow: '1px 1px 2px #ccc',
-            height: '100vh',
-            color: '#000'
-          }}
-          items={items}
-          onClick={handleOnCLick}
-        />
-        <div style={{ flex: 1, padding: '15px 0 15px 15px' }}>
-          <Loading isLoading={memoCount && Object.keys(memoCount) &&  Object.keys(memoCount).length !== 3}>
-            {!keySelected && (
-              <CustomizedContent data={memoCount} colors={COLORS} setKeySelected={setKeySelected} />
-            )}
-          </Loading>
-          {renderPage(keySelected)}
-        </div>
-      </div>
-    </>
-  )
-}
+    const memoCount = useMemo(() => {
+        const result = {};
+        try {
+            if (queries) {
+                console.log(queries)
+                queries.forEach((query) => {
+                    result[query?.data?.key] = query?.data?.data?.length;
+                });
+            }
+            return result;
+        } catch (error) {
+            return result;
+        }
+    }, [queries]);
 
-export default AdminPage
+    const COLORS = {
+        users: ['#e66465', '#9198e5'],
+        products: ['#a8c0ff', '#3f2b96'],
+        orders: ['#11998e', '#38ef7d'],
+        categories: ['#f39c12', '#e74c3c'], // Thêm màu cho danh mục
+    };
+
+    const renderPage = (key) => {
+        switch (key) {
+            case 'users':
+                return <AdminUser />;
+            case 'products':
+                return <AdminProduct />;
+            case 'orders':
+                return <OrderAdmin />;
+            case 'categories':
+                return <CategoryList />; // Thêm trang quản lý danh mục
+            default:
+                return <></>;
+        }
+    };
+
+    const handleOnCLick = ({ key }) => {
+        setKeySelected(key);
+    };
+
+    return (
+        <>
+            <HeaderComponent isHiddenSearch isHiddenCart />
+            <div style={{ display: 'flex', overflowX: 'hidden', marginTop: '92px' }}>
+                <Tabs
+                    style={{
+                        width: '256px',
+                        boxShadow: '1px 1px 2px #ccc',
+                        marginRight: '15px',
+                    }}
+                    onChange={(key) => setKeySelected(key)}
+                >
+                    {items.map((item) => (
+                        <Tabs.TabPane tab={item.title} key={item.key} />
+                    ))}
+                </Tabs>
+                <div style={{ flex: 1, padding: '15px 0 15px 15px' }}>
+                    <Loading
+                        isLoading={
+                            memoCount &&
+                            Object.keys(memoCount) &&
+                            Object.keys(memoCount).length !== items.length
+                        }
+                    >
+                        {!keySelected && (
+                            <CustomizedContent
+                                data={memoCount}
+                                colors={COLORS}
+                                setKeySelected={setKeySelected}
+                            />
+                        )}
+                    </Loading>
+                    {renderPage(keySelected)}
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default AdminPage;
